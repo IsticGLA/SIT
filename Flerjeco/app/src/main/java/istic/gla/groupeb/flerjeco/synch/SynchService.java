c4f8a22eea674b0b3717f774d1d8945ced87df59
amhachi <m.amhachi@gmail.com>
2015-04-13 12:04:40 +0200

    Add: Synch Service

---
+++ Flerjeco/app/src/main/java/istic/gla/groupeb/flerjeco/synch/SynchService.java
@@ -0,0 +1,53 @@
+package istic.gla.groupeb.flerjeco.synch;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * Created by amhachi on 13/04/15.
+ */
+public class SynchService extends IntentService {
+
+    public SynchService()
+    {
+        super("myintentservice");
+    }
+
+
+    Messenger messenger;
+    Timer t=new Timer();
+
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        messenger=(Messenger) intent.getExtras().get("handler");
+
+        t.schedule(new TimerTask() {
+
+            @Override
+            public void run() {
+                // just call the handler every 3 Seconds
+
+                Message msg=Message.obtain();
+                Bundle data=new Bundle();
+                data.putString("k", "value" + System.currentTimeMillis() );
+                msg.setData(data);
+
+                try {
+                    messenger.send(msg);
+                } catch (RemoteException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        }, 100,3000);
+
+    }
+}
