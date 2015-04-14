2478f18fbb6911fc4c61ebf847bab68215c5a6f9
JulesC <jules.chapron@gmail.com>
2015-04-14 09:39:03 +0200

    quit finish the codis request validation

--- Flerjeco/app/src/main/java/istic/gla/groupeb/flerjeco/springRest/SpringService.java
+++ Flerjeco/app/src/main/java/istic/gla/groupeb/flerjeco/springRest/SpringService.java
@@ -141,12 +141,12 @@ public class SpringService {
         return id.getBody();
     }
 
-    public Long changeResourceState(Object[] params) {
+    public Intervention changeResourceState(Object[] params) {
         final String url = URL + "intervention/" + params[0] + "/resources/" + params[1] + "/" + params[2] + "/" + params[3];
         RestTemplate restTemplate = new RestTemplate();
         restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
 
-        ResponseEntity<Long> id = restTemplate.exchange(url, HttpMethod.PUT, null, Long.class);
+        ResponseEntity<Intervention> id = restTemplate.exchange(url, HttpMethod.PUT, null, Intervention.class);
         return id.getBody();
     }
     public StaticData[] getAllStaticDatas() {
