47625a480ddf0bf76e117c016a062fbfabd13456
Nicolas Poirey <nicolas.poirey@gmail.com>
2015-04-16 09:03:43 +0200

    Job de rafraichissement des positions depuis flask

---
+++ nivimoju/drone/engine/src/main/java/istic/gla/goupb/nivimoju/drone/engine/DronePositionRefresherJob.java
@@ -0,0 +1,21 @@
+package istic.gla.goupb.nivimoju.drone.engine;
+
+import org.apache.log4j.Logger;
+import org.quartz.*;
+
+/**
+ * Job refreshing the database with positions from simulation
+ */
+@DisallowConcurrentExecution
+public class DronePositionRefresherJob implements Job{
+    Logger logger = Logger.getLogger(DronePositionRefresherJob.class);
+
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        logger.info("starting job to refresh drone positions");
+        DroneEngine engine = DroneEngine.getInstance();
+        engine.updateDroneInfoFromSimu();
+        logger.info("ending job to refresh drone positions");
+    }
+
+}
